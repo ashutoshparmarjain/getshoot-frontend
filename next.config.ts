3c018bb813aa3337de6d396c26e6a1e9d73de1de
Ashutosh Parmar <ashuparmarjain@gmail.com>
2025-10-29 19:49:33 +0530

    fix: update config to ignore images

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['storage.googleapis.com']
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'storage.googleapis.com',
+        pathname: '/getshoot/uploads/**'
+      }
+    ]
   },
   env: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL
